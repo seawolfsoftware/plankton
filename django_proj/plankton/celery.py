5ae798dd778ce99d3d561ff973a44e20777628ae
Chaz Selph <chaz@seawolfsoftware.io>
2021-01-28 12:33:08 -0500

    before push up

--- django_proj/plankton/celery.py
+++ django_proj/plankton/celery.py
@@ -4,41 +4,35 @@ from celery import Celery
 from celery.schedules import crontab
 
 
-# default django settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plankton.settings')
-# app = Celery('plankton')
-# app.conf.timezone = 'UTC'
-# app.config_from_object("django.conf:settings", namespace="CELERY")
-# app.autodiscover_tasks()
-#
-# app.conf.beat_schedule = {
-#     # executes every 1 minute
-#     'scraping-task-one-min': {
-#         'task': 'scraping.tasks.get_rss',
-#         'schedule': crontab(),
-#     }
-#     # # executes every 15 minutes
-#     # 'scraping-task-fifteen-min': {
-#     #     'task': 'tasks.hackernews_rss',
-#     #     'schedule': crontab(minute='*/15')
-#     # },
-#     # # executes daily at midnight
-#     # 'scraping-task-midnight-daily': {
-#     #     'task': 'tasks.hackernews_rss',
-#     #     'schedule': crontab(minute=0, hour=0)
-#     # }
-# }
 
-app2 = Celery('plankton')
-app2.conf.timezone = 'UTC'
-app2.config_from_object("django.conf:settings", namespace="CELERY")
-app2.autodiscover_tasks()
-args = ["test"]
-app2.conf.beat_schedule = {
+app = Celery('plankton')
+app.conf.timezone = 'UTC'
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
+
+# Can be a list or tuple
+search_headlines_args = ["test"]
+
+app.conf.beat_schedule = {
     # executes every 1 minute
+    'scraping-task-one-min': {
+        'task': 'scraping.tasks.get_rss',
+        'schedule': crontab(),
+    },
     'headlines-task-one-min': {
         'task': 'headlines.tasks.search_headlines',
         'schedule': crontab(),
-        'args': args,
+        'args': search_headlines_args,
     }
+    # # executes every 15 minutes
+    # 'scraping-task-fifteen-min': {
+    #     'task': 'tasks.hackernews_rss',
+    #     'schedule': crontab(minute='*/15')
+    # },
+    # # executes daily at midnight
+    # 'scraping-task-midnight-daily': {
+    #     'task': 'tasks.hackernews_rss',
+    #     'schedule': crontab(minute=0, hour=0)
+    # }
 }
